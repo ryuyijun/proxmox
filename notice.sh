d56da4beed5c9b662f8b01cd0b057b9b8e212c7e
Ryuyijun <ryuyijun@naver.com>
2025-01-18 21:20:28 +0900

    notice.sh 만들기

---
+++ notice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#code from John McLaren
+
+sed -Ezi.bak "s/(function\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
